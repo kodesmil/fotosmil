621d59a24cb2d7c618ccb68a135d48d435db31e7
Marcin Urbański <marcinn.urbanski@gmail.com>
2020-03-15 23:11:02 +0100

    Init

---
+++ gatsby-config.js
@@ -0,0 +1,70 @@
+module.exports = {
+  siteMetadata: {
+    title: `FotoSmil Trondheim`,
+    name: `FotoSmil Trondheim`,
+    siteUrl: `https://fotosmil.no`,
+    description: `This is my description that will be used in the meta tags and important for search results`,
+    hero: {
+      heading: `Professional Photo Booth for events, weddings, parties`,
+      maxWidth: 652,
+    },
+    pricing: {
+      line1: 'Only 3000 Kr/h for the first 2 hours, then 1000 Kr/h for each next'
+    },
+    social: [
+      {
+        name: `twitter`,
+        url: `https://twitter.com/narative`,
+      },
+      {
+        name: `github`,
+        url: `https://github.com/narative`,
+      },
+      {
+        name: `instagram`,
+        url: `https://instagram.com/narative.co`,
+      },
+      {
+        name: `linkedin`,
+        url: `https://www.linkedin.com/company/narative/`,
+      },
+      {
+        name: `dribbble`,
+        url: `https://dribbble.com/narativestudio`,
+      },
+    ],
+  },
+  plugins: [
+    {
+      resolve: "@narative/gatsby-theme-novela",
+      options: {
+        contentPosts: "content/posts",
+        contentAuthors: "content/authors",
+        basePath: "/",
+        authorsPage: true,
+        mailchimp: true,
+        sources: {
+          local: true,
+          contentful: true,
+        },
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Novela by Narative`,
+        short_name: `Novela`,
+        start_url: `/`,
+        background_color: `#fff`,
+        theme_color: `#fff`,
+        display: `standalone`,
+        icon: `src/assets/favicon.png`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-netlify-cms`,
+      options: {
+      },
+    },
+  ],
+};
