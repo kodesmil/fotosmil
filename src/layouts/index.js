c07cc2bafeef6228cb3976048bf89d7630e2ed1e
Marcin Urbański <marcinn.urbanski@gmail.com>
2020-03-23 18:55:17 +0100

    Fix assets

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,5 +1,4 @@
 import React from "react"
-import { TransitionProvider, TransitionViews } from "gatsby-plugin-transitions"
 
 const Layout = ({ location, children }) => {
   return <div location={location}>{children}</div>
